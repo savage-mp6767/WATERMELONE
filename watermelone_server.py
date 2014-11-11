a61d8864886f27590b40a61e7b984449f555cce3
savage-mp6767 <liegrowlmuzak@gmail.com>
2014-11-10 23:09:43 -0600

    Revert "threaded server"
    This reverts commit ce6da53f04547de7baa0856fa2d2536b54a2015a.

--- watermelone_server.py
+++ watermelone_server.py
@@ -4,44 +4,19 @@
 
 import socket
 import sys
-import threading
-
-class ClientThread ( threading.Thread ):
-   # Override Thread's __init__ method to accept the parameters needed:
-   def __init__ ( self, channel, details ):
-      self.channel = channel
-      self.details = details
-      threading.Thread.__init__ ( self )
-
-   def run ( self ):
-      print 'Received connection:', self.details [ 0 ]
-      for x in xrange ( 10 ):
-         data = self.channel.recv ( 1024 )
-         if not data:
-             break
-         else:
-             print data
-      self.channel.close()
-      print 'Closed connection:', self.details [ 0 ]
-   
-   def sendData(self,data):
-       self.channel.send(data)
 
 class WMServer:
     def __init__(self):
-        self.server_addr = ('localhost',12347)
+        self.server_addr = ('localhost',12342)
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.bind(self.server_addr)
         self.connection.listen(5)
-        self.Update()
-        self.clients = []
     
     def Update(self):
-        channel, details = self.connection.accept()
-        ClientThread ( channel, details ).start()
+        print 'waiting for a connection'
+        (c, client_address) = self.connection.accept()
+        print str(client_address) + 'connected'
         self.Update()
-    
-    def GetClients(self):
-        return self.clients
 		
 server = WMServer()
+server.Update()
